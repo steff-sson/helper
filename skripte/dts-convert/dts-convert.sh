2e22cf3dfd2ee03ea1eff59ad0b90eed30b5294b
stefan <stefan@weitzelnet.com>
2024-05-05 19:59:32 +0200

    dts-convert now searches for eac3 streams also

--- skripte/dts-convert/dts-convert.sh
+++ skripte/dts-convert/dts-convert.sh
@@ -39,6 +39,26 @@ do
    mv "${f%.mkv}-ac3.mkv" "$f"
    echo "$date: Fertig!" >> $log
  fi
+ if ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of csv=p=0 "$f" | grep eac3;
+ then
+   ((dts_check=dts_check+1))
+   echo "$date: $f gefunden und gespeichert, starte AC3 Re-Encode..." >> $log
+   ffmpeg -i "$f" -map 0 -map -0:s -c:v copy -c:a ac3 -b:a 640k "${f%.mkv}-ac3.mkv"
+   echo "$date: AC3 Re-Encode abgeschlossen. Entferne Original-MKV und ersetze sie durch neue MKV." >> $log
+   # rm "$f"
+   mv "${f%.mkv}-ac3.mkv" "$f"
+   echo "$date: Fertig!" >> $log
+ fi
+ if ffprobe -v error -select_streams a:1 -show_entries stream=codec_name -of csv=p=0 "$f" | grep eac3;
+ then
+   ((dts_check=dts_check+1))
+   echo "$date: $f gefunden und gespeichert, starte AC3 Re-Encode..." >> $log
+   ffmpeg -i "$f" -map 0 -map -0:s -c:v copy -c:a ac3 -b:a 640k "${f%.mkv}-ac3.mkv"
+   echo "$date: AC3 Re-Encode abgeschlossen. Entferne Original-MKV und ersetze sie durch neue MKV." >> $log
+   # rm "$f"
+   mv "${f%.mkv}-ac3.mkv" "$f"
+   echo "$date: Fertig!" >> $log
+ fi
 done
 
 if [[ $dts_check -eq 0 ]]
